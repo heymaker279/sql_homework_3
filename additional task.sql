92e69194c86be441e065fe6d87c257d258195a08
heyma <heymaker@yandex.ru>
2023-01-14 17:56:12 +0600

    add readme

--- additional task.sql
+++ additional task.sql
@@ -1,14 +1,16 @@
 create table if not exists Employee(
 	id serial primary key,
-	name_ varchar(80) not null	
+	name_ varchar(80) not null
 );
 
 create table if not exists Department(
-	department_name varchar(80) primary key 
+	id serial primary key,
+	department_name varchar(80) not null
 );
 
 create table if not exists Chief(
-	department varchar(80) not null references Department(department_name),
+	id serial prymary key,
+	department varchar(80) not null references Department(id),
 	employee_id integer not null references Employee(id),
 	constraint pk primary key (department, employee_id)
 );
